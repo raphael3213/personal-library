587ca95b677cd4e3509f94c5ff8e7c01727838bb
Glitch (personal-library12214) <none>
2018-06-13 16:59:43 +0000

    🍧😋 Checkpoint ./public/client.js:1174538/506

--- public/client.js
+++ public/client.js
@@ -10,11 +10,31 @@
       datatype:"json",
       success: function(data){
       var str;
+        var str1="";
       console.log(data);
       data.forEach(function(ele){
+        
+        if(ele.comment.length!=0){
+        
+        ele.comment.forEach(function(el,i){
+        
+        str1+="<p>"
+        })
+        
+        
+        }
+        
+      str=["<div id='booker'>",
+           "<h3>Title:","<br>",
+           ele.title,"</h3>",
+           "<h3>BOOK ID:",
+           ele.book_id,"</h3>",
+           
+           
+           
+           "</div>"
       
-      
-      
+      ]
       
       })
       
