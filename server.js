e339dbdad4838b3d3601c82a28abd4bd76946678
Glitch (personal-library12214) <none>
2018-06-13 14:35:17 +0000

    🐴👣 Checkpoint ./server.js:1174538/374

--- server.js
+++ server.js
@@ -4,11 +4,14 @@
 // init project
 var express = require('express');
 var app = express();
+var mongoose=require('mongoose')
+var bp=require('body-parser');
+var helmet=require('helmet');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+app.use(bp.json())
+app.use(bp.urlencoded({extended:false}));
+app.use(helmet());
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
